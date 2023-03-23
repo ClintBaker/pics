cf9439bf3cbb1033be051e428cf7483bca9f1cdf
CLINT <clint@jlbcompanies.com>
2023-03-23 15:39:17 -0600

    finalized

--- src/api.js
+++ src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID Ufwa0DSF7kgTgtq1zYksHmuxw5_pYcQi7P_7Kcnxft4'
         },
         params: {
-            query: 'cars'
+            query: term
         },
     });
-
-    console.log(response);
-    return response;
+    
+    return response.data.results;
 };
 
 export default searchImages;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,28 @@
-function SearchBar() {
-    return <div>SearchBar</div>;
-}
+import { useState } from 'react';
+import './SearchBar.css';
+
+function SearchBar({ onSubmit }) {
+    const [ term, setTerm ] = useState(''); 
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        onSubmit(term);
+        setTerm('');
+    };
+
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+    };
+
+    return (
+        <div className="search-bar">
+            <form onSubmit={handleFormSubmit}>
+                <label>Enter search term</label>
+                <input value={term} onChange={handleChange} />
+            </form>
+        </div>
+    );
+};
 
 export default SearchBar;
